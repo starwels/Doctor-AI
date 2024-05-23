86e42e62de8a77728f3152177040dd1288913cb7
Wellington Santos <wlss.lucas@gmail.com>
2024-05-22 21:50:31 -0300

    Improve chats index page

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -9,7 +9,7 @@ class ChatsController < ApplicationController
     #   @response = 'response is empty'
     # end
 
-    @chats = current_user.chats
+    @chats = current_user.chats.order(created_at: :desc)
   end
 
   def show
@@ -23,7 +23,7 @@ class ChatsController < ApplicationController
 
     assistant = Assistant.find_by(name: params[:assistant])
 
-    @chat.name = "Chat - #{params[:assistant]} - #{l(Date.current)}"
+    @chat.name = "Chat - #{params[:assistant]} - #{l(Date.current.in_time_zone('Brasilia'))}"
     @chat.assistant = assistant
     @chat.thread_id = thread_id
 
