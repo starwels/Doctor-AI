1a41aa5012fad6a94bf6fcdc06c57a5150880168
Wellington Santos <wlss.lucas@gmail.com>
2024-06-03 01:32:58 -0300

    Fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,14 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-u = User.create!(email: 'admin@doctorai.com', password: '10203040', password_confirmation: '10203040')
-u.skip_confirmation!
-u.save!
+u1 = User.new(email: 'admin@doctorai.com', password: '10203040', password_confirmation: '10203040')
+u1.skip_confirmation!
+u1.save!
+
+u2 = User.new(email: 'renan@doctorai.com', password: '2024@doctorAI', password_confirmation: '2024@doctorAI')
+u2.skip_confirmation!
+u2.save!
+
 
 Assistant.create!(name: 'Resumex', external_id: 'asst_Hdq7fmjkboQOa62NhdXlZQPt')
 Assistant.create!(name: 'GreenMinds', external_id: 'asst_RFBLKJt7O5jSLm8hO5HtGE27')
