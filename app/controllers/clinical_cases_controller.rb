362585c46cc1ca1c26144723879408c0b11d4a8a
Wellington Santos <wlss.lucas@gmail.com>
2024-06-03 13:41:04 -0300

    Update clinical case prompt

--- app/controllers/clinical_cases_controller.rb
+++ app/controllers/clinical_cases_controller.rb
@@ -15,18 +15,27 @@ class ClinicalCasesController < ApplicationController
       @clinical_case.patient_data = clinical_case_params
       @clinical_case.save!
 
-      body = <<-HTML
-Me ajude na avaliação do seguinte caso clínico:
-<div class="d-grid gap-2">
-<a href="#{url_for([@chat, @clinical_case])}" data-turbo="false" class="btn btn-primary">Link para o Caso</a>
-</div>
+      body = <<~HTML
+        Me forneça uma avaliação completa e minuciosa do seguinte caso clínico.
+        Pondere os potenciais benefícios e riscos do tratamento com cannabis para este paciente.
+        Sugira ou não um modelo de prescrição incluindo tipo do produto, composição, posologia e cuidados importantes.
+        Por fim, monte um plano de monitoramento da eficácia do tratamento para este paciente.
+        <div class="d-grid gap-2">
+        <a href="#{url_for([@chat, @clinical_case])}" data-turbo="false" class="btn btn-primary">Link para o Caso</a>
+        </div>
       HTML
 
       @chat.messages.create!(body: body, origin: :user)
       @response = @chat.messages.create!(body: '', origin: :ai)
     end
 
-    prompt = 'Me ajude na avaliação do seguinte caso clínico: ' + clinical_case_params.to_json
+    prompt = <<~PROMPT
+      Me forneça uma avaliação completa e minuciosa do seguinte caso clínico.
+      Pondere os potenciais benefícios e riscos do tratamento com cannabis para este paciente.
+      Sugira ou não um modelo de prescrição incluindo tipo do produto, composição, posologia e cuidados importantes.
+      Por fim, monte um plano de monitoramento da eficácia do tratamento para este paciente. Caso clínico:
+      #{clinical_case_params.to_json}
+    PROMPT
 
     AiResponseJob.perform_later(prompt: prompt, message: @response)
 
