1ff585ec6122f084cc335fc089bcc1ca2f8dab99
Wellington Santos <wlss.lucas@gmail.com>
2024-06-04 19:42:28 -0300

    Add user messages association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
          :confirmable, :lockable, :trackable
 
   has_many :chats, dependent: :nullify
+  has_many :messages, through: :chats, dependent: :nullify
 
   enum role: { user: 0, admin: 1 }
 end
