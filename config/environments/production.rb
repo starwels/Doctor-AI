bd26bf3c472cf6a0a721732b98b1f972d2c8a120
Wellington Santos <wlss.lucas@gmail.com>
2024-06-19 16:55:10 -0300

    Update host mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.default_url_options = { host: 'greenmindsai.com.br' }
+  config.action_mailer.default_url_options = { host: 'app.greenmindsai.com.br' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
